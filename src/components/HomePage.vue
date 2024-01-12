4b32dc424a00318349f2adef082bc2b60cfe896b
Lucas Matheus <lucas.peres@outlook.com>
2024-01-12 11:02:01 -0300

    Committ

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,23 +1,35 @@
 <template>
     <HeaderPage></HeaderPage>
-    <h1>Hello User</h1>
+    <h1>Hello {{ userName }}</h1>
 </template>
 
 <script>
-    import HeaderPage from './HeaderPage.vue';
-    export default {
-        name:'HomePage',
-        components:{
-            HeaderPage
-        },
-        mounted() {
-            let user = localStorage.getItem('user-info')
-            if(!user) {
-                this.$router.push({name:"SignUp"})
-            }
+import HeaderPage from './HeaderPage.vue';
+export default {
+    name: 'HomePage',
+    components: {
+        HeaderPage
+    },
+
+    data() {
+        return {
+            userName: ''
+        }
+    },
+
+    mounted() {
+        let user = localStorage.getItem('user-info')
+        console.log(user)
+
+        if (!user) {
+            this.$router.push({ name: "SignUp" })
+        } else {
+            user = JSON.parse(user);
+
+            this.userName = user[0].nome;
         }
     }
+}
 </script>
 
-<style>
-</style>
+<style></style>
